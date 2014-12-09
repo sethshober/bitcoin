2b76a32d9d759ecdf73bcdea0c582aec5db19939
Seth Shober <seth.shober@gmail.com>
2014-12-08 22:40:04 -0800

    bind bitcoin input to DOM output

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var bitcoin = new Firebase("https://publicdata-cryptocurrency.firebaseio.com/bitcoin");
+var bitcoinRef = new Firebase("https://publicdata-cryptocurrency.firebaseio.com/bitcoin"),
+    prevPrice = 0,
+    bitcoinInput = document.getElementById('bitcoin-input'),
+    //dollarInput = document.getElementById('dollar-input'),
+    bitcoinPriceOutput = document.getElementById('current-price'),
+    moniesOutput = document.getElementById('monies');
 
-bitcoin.child("last").on("value", updatePrice);
 
-bitcoin.child("_updated").on("value", updatedLast);
+//bitcoinRef.child('last').once("value", setDollarsInput)
 
-var prevPrice = '';
+bitcoinRef.child("last").on("value", updatePrice);
+
+bitcoinRef.child("_updated").on("value", updatedLast);
+
+
+bitcoinInput.onkeyup = calculateMonies; // listen for keyup on bitcoinInput and run calculateMonies function 
+//dollarInput.onkeyup = calculateMonies;
 
 
 // show current price of bitcoin in US dollars
@@ -18,19 +28,21 @@ function updatePrice(snapshot) {
 	
 	console.log(prevPrice);
 
-	price = $('#current-price').text("$" + snapPrice);
+	price = $('#current-price').text("$" + snapPrice);	
 
-	if(price > prevPrice || prevPrice == 0){
-		price.effect('highlight', {color: 'lightgreen'});
-	} else {
+	if(snapPrice < prevPrice){
 		price.effect('highlight', {color: 'tomato'});
+	} else {
+		price.effect('highlight', {color: 'lightgreen'});
 	}
 
+	
 
 	//document.getElementById('current-price').innerHTML = "1&#x243; = $" + snapshot.val();
 	
 	console.log(snapPrice);
 
+	calculateMonies();
 
 }
 
@@ -68,6 +80,41 @@ function convertUTCDateToLocalDate(date) {
 
     return newDate;   
 }
+
+// calculate value of bitcoin, return it, and add to DOM
+function calculateMonies() {
+
+	var bitcoins = Number(bitcoinInput.value);
+	//var dollars = Number(dollarInput.value);
+	var bitcoinPrice = Number(bitcoinPriceOutput.innerHTML.slice(1)); //remove dollar sign with slice
+	var monies;
+	
+	console.log("bitcoins: " + bitcoins);
+	//console.log("dollars: " + dollars);
+	console.log("bitcoinPrice: " + bitcoinPrice);
+	
+	monies = (bitcoins * bitcoinPrice).toFixed(2); //keep to two decimal places with toFixed(2)
+
+	monies = "$" + addCommasToNumber(monies);
+
+	moniesOutput.innerHTML = monies;
+	
+	console.log(monies);
+
+	return monies;
+
+}
+
+// function setDollarsInput(snapshot) {
+// 	dollarInput.value = snapshot.val();
+// 	calculateMonies();
+// }
+
+// format number with commas
+function addCommasToNumber(n) {
+	return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 
 // slot machine number change effect
 // function slotmachine(snapshot,id) {
